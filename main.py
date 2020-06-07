e48cbf41427a835adb4ad9e095f9ff0907996279
Arlissonjaques <arlissonjaques@gmail.com>
2020-06-07 15:20:20 -0400

    Comentarios adicionados

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ import os
 lista_com_extensao = []
 
 def remove_arquivos():
+       
+    for arquivo in os.listdir(argv[1]): # Corre em todos os arquivos do diretorio
+        extensao_arquivo = os.path.splitext(arquivo)[1] # Pega a extensão do arquivo
 
-    for arquivo in os.listdir(argv[1]):
-        extensao_arquivo = os.path.splitext(arquivo)[1]
-
-        if extensao_arquivo in lista_com_extensao:
-            os.remove(argv[1] + '/' + arquivo)
+        if extensao_arquivo in lista_com_extensao: # Caso a extensão seja igual a especificada no argumento
+            os.remove(argv[1] + '/' + arquivo) # Exclui o arquivo
 
 
 #------------------------------------------------------------------------------------
 
 def mensagens(lista):
-
+    """
+    Função basica de feedback ao usuario.
+    """
     tamanho = len(lista)
 
     resposta = input(f"Existem {tamanho} arquivos repetidos a serem apagados, continuar(y/n):\n >>> ").lower()
@@ -35,11 +37,11 @@ def mensagens(lista):
 
 def main():
     
-    for nome_arquivo in os.listdir(argv[1]):
-        extensao = os.path.splitext(nome_arquivo)[1]
+    for nome_arquivo in os.listdir(argv[1]): # Corre em todos os arquivos do diretorio
+        extensao = os.path.splitext(nome_arquivo)[1] # Pega a extensão do arquivo
 
-        if extensao == argv[2]:
-            lista_com_extensao.append(extensao)
+        if extensao == argv[2]: # Caso a extensão seja igual a especificada no argumento
+            lista_com_extensao.append(extensao) # Adiciona na lista
 
     mensagens(lista_com_extensao)
 
